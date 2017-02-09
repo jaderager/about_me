ffc3621694eddf403afce44b5a9f340d9d0ac525
Morgan Fogarty <mofo37@gmail.com>
2017-02-09 14:58:33 -0800

    Added function to lines 10 and 28

--- app.js
+++ app.js
@@ -7,70 +7,83 @@ if (userOptIn === "Y") {
     var questionsCorrect = 0;
     for (var i = 1; i <=5; i++) {
         var promptOutput;
+        function setOutput(x) {
+            promptOutput = x;
+        }
         if (i === 1) {
-            var promptOutput = "Is or is not Jade a kitteh fanatic?";
+            setOutput("Is or is not Jade a kitteh fanatic?");
         } else if (i === 2) {
-            var promptOutput = "Is Jade bad at posing unprompted questions or trivia?";
+            setOutput("Is Jade bad at posing unprompted questions or trivia?");
         } else if (i === 3) {
-            var promptOutput = "Does Jade like the cold?";
+            setOutput("Does Jade like the cold?");
         } else if (i === 4) {
-            var promptOutput = "Has Jade ever been to another country?";
+            setOutput("Has Jade ever been to another country?");
         } else if (i === 5) {
-            var promptOutput = "Is Jade getting enough sle...zzZ";
+            setOutput("Is Jade getting enough sle...zzZ");
         }
 
         var input = prompt(promptOutput).toUpperCase();
         console.log("A" + i + ": " + input);
 
-        if (i === 1) {
-            if (input === "Y") {
-                alert("Correct!");
-                questionsCorrect++;
-            } else if (input === "N") {
-                alert("Newp.");
-            } else {
-                alert("Invalid input.");
-            }
-        } else if (i === 2) {
-            if (input === "Y") {
-                alert("Correct!");
-                questionsCorrect++;
-            } else if (input === "N") {
-                alert("Newp.");
-            } else {
-                alert("Invalid input.");
-            }
-        } else if (i === 3) {
-            if (input === "Y") {
-                alert("Newp.");
-            } else if (input === "N") {
-                alert("Correct!");
-                questionsCorrect++;
-            } else {
-                alert("Invalid input.");
-            }
-        } else if (i === 4) {
-            if (input === "Y") {
-                alert("Newp.");
-            } else if (input === "N") {
-                alert("Correct!");
-                questionsCorrect++;
-            } else {
-                alert("Invalid input.");
-            }
-        } else if (i === 5) {
-            if (input === "Y") {
-                alert("zzZ...");
-            } else if (input === "N") {
-                alert("zzZ...");
-                questionsCorrect++;
-            } else {
-                alert("zzzzZ... quit breaking my scripts...zzZ");
-            }
+        function checkInput(iteration, correctResponse, incorrectResponse, correctInput, incorrectInput) {
+            if (iteration === 1) {
+                if (input === correctInput) {
+                    alert(correctResponse);
+                    questionsCorrect++;
+                } else if (input === incorrectInput) {
+                    alert(incorrectResponse);
+                } else {
+                    alert("Invalid input.");
+                }
+            } 
+        } 
+
+        checkInput(1, "Correct!", "Newp.", "Y", "N");
+        checkInput(2, "Correct!", "Newp.", "Y", "N");
+        checkInput(3, "Correct!", "Newp.", "N", "Y");
+        checkInput(4, "Correct!", "Newp.", "N", "Y");
+        checkInput(5, "Correct!", "Newp.", "N", "Y");
+
+        // else if (i === 2) {
+        //     if (input === "Y") {
+        //         alert("Correct!");
+        //         questionsCorrect++;
+        //     } else if (input === "N") {
+        //         alert("Newp.");
+        //     } else {
+        //         alert("Invalid input.");
+        //     }
+        // } else if (i === 3) {
+        //     if (input === "Y") {
+        //         alert("Newp.");
+        //     } else if (input === "N") {
+        //         alert("Correct!");
+        //         questionsCorrect++;
+        //     } else {
+        //         alert("Invalid input.");
+        //     }
+        // } else if (i === 4) {
+        //     if (input === "Y") {
+        //         alert("Newp.");
+        //     } else if (input === "N") {
+        //         alert("Correct!");
+        //         questionsCorrect++;
+        //     } else {
+        //         alert("Invalid input.");
+        //     }
+        // } else if (i === 5) {
+        //     if (input === "Y") {
+        //         alert("zzZ...");
+        //     } else if (input === "N") {
+        //         alert("zzZ...");
+        //         questionsCorrect++;
+        //     } else {
+        //         alert("zzzzZ... quit breaking my scripts...zzZ");
+        //     }
         }
     }
     userOptIn = "6"; //advance execution
-}
+
 
 if (userOptIn === "6") { //debug feature; jump to 6th question
 
